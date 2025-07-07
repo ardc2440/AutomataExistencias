1b1c8f494355199aa7de6178d6a180bb8276e296
ardc2440@gmail.com <ardc2440@gmail.com>
2025-07-06 22:14:33 -0500

    Prueba de Nuevo GIT

--- App/AutomataExistencias/AutomataExistencias.DataAccess/Aldebaran/Stock.cs
+++ App/AutomataExistencias/AutomataExistencias.DataAccess/Aldebaran/Stock.cs
@@ -20,7 +20,7 @@ namespace AutomataExistencias.DataAccess.Aldebaran
         public short StorageCellarId { get; set; }
 
         [Column("COLOR")]
-        public string Color { get; set; }
+        public string Colores { get; set; }
 
         [Column("CANTIDAD")]
         public int Quantity { get; set; }
